ed6e2385f613db5acb106417965ecd2452eefccb
Shania Mansoori <shaniabca2024@gmail.com>
2026-07-27 23:26:09 +0530

    update API URL for production

--- server/server.js
+++ server/server.js
@@ -14,7 +14,10 @@ const app = express();
 
 app.use(
     cors({
-       origin: "http://localhost:5173",
+       origin: [
+            "http://localhost:5173",
+            "https://mern-auth-app-pu25.onrender.com/",
+        ],
     credentials: true,
     })
 );
